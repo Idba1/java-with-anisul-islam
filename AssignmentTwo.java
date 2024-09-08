e77bcdeb03b8d79218375df6a142e0a6e5a906f4
Idba1 <moniraislamself@gmail.com>
2024-09-08 11:04:18 +0600

    assignmentTwo

---
+++ AssignmentTwo.java
@@ -0,0 +1,19 @@
+/**
+ * AssignmentTwo
+ */
+public class AssignmentTwo {
+
+    public static void main(String[] args) {
+        int id = 101;
+        String title = "iphone15";
+        String price = "1895 euro";
+        String description = "product with the best photo quality";
+        String category = "phone";
+
+        System.out.println("id is " + id);
+        System.out.println("phone name is " + title);
+        System.out.println("price is " + price);
+        System.out.println("description is " + description);
+        System.out.println("category is " + category);
+    }
+}
