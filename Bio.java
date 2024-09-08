05928e263228c3ccec1c01f4d7670f9b4f625e85
Idba1 <moniraislamself@gmail.com>
2024-09-08 09:49:08 +0600

    using escape and comments

--- Bio.java
+++ Bio.java
@@ -3,5 +3,12 @@ public class Bio {
     public static void main(String[] args){
         System.out.println("Idba Islam");
         System.out.println("+8801618360986");
+
+        // using Escape sequence
+        System.out.println("Idba Islam\nand my phn no in+8801618360986");
+        System.out.println("1\t2\t3\t4");
+        System.out.println("\"java programming\"");
+
+        // using comments
     }
 }
